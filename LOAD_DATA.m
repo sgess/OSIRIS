65a826b9b2c396e7316273667b1caa67b870fca9
sgess <spencergessner@gmail.com>
2012-09-09 16:08:13 -0700

    ana updates

--- LOAD_DATA.m
+++ LOAD_DATA.m
@@ -1,19 +1,3 @@
-function data = LOAD_DATA(file)
+function data = LOAD_DATA(file,loc)
 
-if ~isempty(strfind(file,'charge-'))
-    loc = '/charge';
-elseif ~isempty(strfind(file,'e1-'))
-    loc = '/e1';
-elseif ~isempty(strfind(file,'e2-'))
-    loc = '/e2';
-elseif ~isempty(strfind(file,'e3-'))
-    loc = '/e3';
-elseif ~isempty(strfind(file,'b1-'))
-    loc = '/b1';
-elseif ~isempty(strfind(file,'b2-'))
-    loc = '/b2';
-elseif ~isempty(strfind(file,'b3-'))
-    loc = '/b3';
-end
-
-data = h5read(file,loc); 
+data = h5read(file,['/' loc]); 
--- OSANA.m
+++ OSANA.m
@@ -3,43 +3,107 @@
 
 clear all;
 
-data_dir = '/Users/sgess/Desktop/FACET/os_tars/2012/Sep/07/OS_pShort2/';
+data_dir = '/Users/sgess/Desktop/FACET/os_tars/';
+plot_dir = '/Users/sgess/Desktop/FACET/OS_PLOTS/';
+date_dir = '2012/Sep/07/';
+set_dir  = 'OS_pShort2/';
+plot_dir = '/Users/sgess/Desktop/FACET/OS_PLOTS/';
 
-beam_rho_file = 'MS/DENSITY/beam/charge/charge-beam-003128.h5';
-plas_rho_file = 'MS/DENSITY/plasma/charge/charge-plasma-003128.h5';
-field_e1_file = 'MS/FLD/e1/e1-003128.h5';
-field_e2_file = 'MS/FLD/e2/e2-003128.h5';
-field_b3_file = 'MS/FLD/b3/b3-003128.h5';
+data_loc = [data_dir date_dir set_dir];
+plot_loc = [plot_dir date_dir set_dir];
+if(~exist(plot_loc,'dir'))
+    mkdir(plot_loc);
+end
+
+file_number = 3128;
+num_str = num2str(file_number,'%06d');
+
+bp_file = ['MS/DENSITY/beam/charge/charge-beam-' num_str '.h5'];
+pp_file = ['MS/DENSITY/plasma/charge/charge-plasma-' num_str '.h5'];
+e1_file = ['MS/FLD/e1/e1-' num_str '.h5'];
+e2_file = ['MS/FLD/e2/e2-' num_str '.h5'];
+b3_file = ['MS/FLD/b3/b3-' num_str '.h5'];
+
+bp_type = 'charge';
+pp_type = 'charge';
+e1_type = 'e1';
+e2_type = 'e2';
+b3_type = 'b3';
 
 % Load beam charge density
-beam_rho = LOAD_DATA([data_dir beam_rho_file]);
-plas_rho = LOAD_DATA([data_dir plas_rho_file]);
-field_e1 = LOAD_DATA([data_dir field_e1_file]);
-field_e2 = LOAD_DATA([data_dir field_e2_file]);
-field_b3 = LOAD_DATA([data_dir field_b3_file]);
+beam_rho = LOAD_DATA([data_loc bp_file],bp_type);
+plas_rho = LOAD_DATA([data_loc pp_file],pp_type);
+field_e1 = LOAD_DATA([data_loc e1_file],e1_type);
+field_e2 = LOAD_DATA([data_loc e2_file],e2_type);
+field_b3 = LOAD_DATA([data_loc b3_file],b3_type);
 
-[z_axis, r_axis] = LOAD_AXIS([data_dir beam_rho_file]);
+[z_axis, r_axis] = LOAD_AXIS([data_loc bp_file]);
 
-figure;
-imagesc(abs(log(flipdim(plas_rho',1))));
-colorbar;
+RR = linspace(r_axis(1),r_axis(2),length(beam_rho));
+ZZ = linspace(z_axis(1),z_axis(2),length(beam_rho)) - z_axis(1);
 
-figure;
-imagesc(abs(log(flipdim(beam_rho',1))));
-colorbar;
+% figure;
+% imagesc(ZZ,RR,abs(log(flipdim(plas_rho',1))));
+% colorbar;
+% xlabel('c/\omega_p','fontsize',16);
+% ylabel('c/\omega_p','fontsize',16);
+% t = colorbar('peer',gca);
+% set(get(t,'ylabel'),'String', 'n_0','fontsize',16);
+% title('Log of Plasma Density','fontsize',16);
+% 
+% figure;
+% imagesc(ZZ,RR,abs(log(flipdim(beam_rho',1))));
+% colorbar;
+% xlabel('c/\omega_p','fontsize',16);
+% ylabel('c/\omega_p','fontsize',16);
+% t = colorbar('peer',gca);
+% set(get(t,'ylabel'),'String', 'n_0','fontsize',16);
+% title('Log of Beam Density','fontsize',16);
 
-figure;
-imagesc(flipdim(plas_rho',1));
-colorbar;
+% figure;
+% imagesc(ZZ,RR(122)-RR(2:122),flipdim(plas_rho(:,2:122)',1));
+% colorbar;
+% %caxis([min(min(plas_rho(:,2:122)))/10 0]);
+% xlabel('c/\omega_p','fontsize',16);
+% ylabel('c/\omega_p','fontsize',16);
+% t = colorbar('peer',gca);
+% set(get(t,'ylabel'),'String', 'n_0','fontsize',16);
+% title('Plasma Density','fontsize',16);
+% v =  axis;
+% text(3*v(2)/5,5*v(4)/6,'Beam Direction \rightarrow','FontSize',16,'FontWeight','bold');
+% %saveas(gca,[plot_dir date_dir set_dir 'plas_rho.pdf']);
 
-figure;
-imagesc(flipdim(beam_rho',1));
-colorbar;
-
-figure;
-imagesc(flipdim(field_e1',1));
-colorbar;
-
-figure;
-plot(field_e1(:,1));
-
+% figure;
+% imagesc(ZZ,RR(122)-RR(2:122),flipdim(beam_rho(:,2:122)',1));
+% colorbar;
+% %caxis([0 max(max(beam_rho(:,2:122)))/10]);
+% xlabel('c/\omega_p','fontsize',16);
+% ylabel('c/\omega_p','fontsize',16);
+% t = colorbar('peer',gca);
+% set(get(t,'ylabel'),'String', 'n_0','fontsize',16);
+% title('Beam Density','fontsize',16);
+% v =  axis;
+% text(3*v(2)/5,5*v(4)/6,'Beam Direction \rightarrow','FontSize',16,'FontWeight','bold');
+% %saveas(gca,[plot_dir date_dir set_dir 'beam_rho.pdf']);
+% 
+% figure;
+% imagesc(ZZ,RR(122)-RR(2:122),flipdim(field_e1(:,2:122)',1));
+% colorbar;
+% xlabel('c/\omega_p','fontsize',16);
+% ylabel('c/\omega_p','fontsize',16);
+% t = colorbar('peer',gca);
+% set(get(t,'ylabel'),'String', 'm c \omega_p / e','fontsize',16);
+% title('Longitudinal E Field','fontsize',16);
+% v =  axis;
+% text(3*v(2)/5,5*v(4)/6,'Beam Direction \rightarrow','FontSize',16,'FontWeight','bold');
+% %saveas(gca,[plot_dir date_dir set_dir 'EZ_2D.pdf']);
+% 
+% figure;
+% plot(ZZ,field_e1(:,2));
+% xlabel('c/\omega_p','fontsize',16);
+% ylabel('m c \omega_p / e','fontsize',16);
+% title('Longitudinal E Field on Axis','fontsize',16);
+% v =  axis;
+% text(2*v(2)/3,4*v(3)/5,'Beam Direction \rightarrow','FontSize',16,'FontWeight','bold');
+% %saveas(gca,[plot_dir date_dir set_dir 'EZ_1D.pdf']);
+save('../COMPARE/OS_pShort2.mat','ZZ','field_e1');
