15dc7b233fa63d4d4a55f7f7cdf5badb0fc2e57f
sgess <spencergessner@gmail.com>
2014-01-15 00:37:09 -0800

    kinosim

--- MAKE_OS2.m
+++ MAKE_OS2.m
@@ -27,7 +27,7 @@ if ~exist(command_dir,'dir')
     mkdir(command_dir);
 end
 
-osinput_output_name = 'constSZ_70';
+osinput_output_name = 'kinoSim';
 osinput_output_file = [osinput_dir 'os-stdin_' osinput_output_name];
 
 write = 1;
@@ -63,16 +63,16 @@ input_struct.plasma.mass       = SI_eM/SI_eM;     % Particle mass in units of el
 
 % hollow channel profile
 input_struct.hollow.use        = 1;               % use hollow channel?
-input_struct.hollow.radius     = 70;              % central radius in microns
-input_struct.hollow.width      = 10;              % annulus width in microns
+input_struct.hollow.radius     = 77.6;              % central radius in microns
+input_struct.hollow.width      = 30;              % annulus width in microns
 input_struct.hollow.ramp       = 0.1;             % ramp length in microns
 input_struct.hollow.n_points   = 6;               % number of points in profile
-input_struct.hollow.type       = 'hollow';        % profile type
+input_struct.hollow.type       = 'flat';        % profile type
 
 % beam parameters
 input_struct.beam.charge       = -1.0;            % -1 for electron, +1 for positron
 input_struct.beam.mass         = SI_eM/SI_eM;     % Particle mass in units of electron mass
-input_struct.beam.N_particles  = 1.96e9;          % Number of beam particles
+input_struct.beam.N_particles  = 1.00e10;          % Number of beam particles
 input_struct.beam.gamma        = 40000;           % relativistic factor gamma, if 0 energy specified below
 input_struct.beam.sigma_r      = 10.;             % Gaussian sigma_x [um]
 input_struct.beam.sigma_z      = 20.0;            % Gaussian sigma_z [um]
--- OSANA.m
+++ OSANA.m
@@ -3,13 +3,13 @@
 
 clear all;
 
-savE = 1;
+savE = 0;
 
-%data_dir = '/Users/sgess/Desktop/FACET/os_tars/';
-%plot_dir = '/Users/sgess/Desktop/FACET/OS_PLOTS/';
+data_dir = '/Users/sgess/Desktop/FACET/os_tars/';
+plot_dir = '/Users/sgess/Desktop/FACET/OS_PLOTS/';
 
-data_dir = '/Users/sgess/Desktop/data/os_tars/';
-plot_dir = '/Users/sgess/Desktop/plots/OS/';
+%data_dir = '/Users/sgess/Desktop/data/os_tars/';
+%plot_dir = '/Users/sgess/Desktop/plots/OS/';
 
 %date_dir = '2012/Sep/07/';
 %date_dir = '2013/Mar/29/';
@@ -19,7 +19,7 @@ plot_dir = '/Users/sgess/Desktop/plots/OS/';
 %date_dir = '2013/May/06/';
 %date_dir = '2013/Sep/04/';
 %date_dir = '2013/now/';
-date_dir = '2014/Jan/09/'; date_par = '2014/Jan/09/';
+date_dir = '2014/Jan/08/'; date_par = '2014/Jan/08/';
 
 %set_dir = 'OS_eShort2/';
 %set_dir  = 'hollow2/';
@@ -46,7 +46,7 @@ date_dir = '2014/Jan/09/'; date_par = '2014/Jan/09/';
 %set_dir = 'wtest3/'; plot_name = 'sd_10';  % 1.0 sd
 %set_dir = 'wtest4/'; plot_name = 'sd_05';  % 0.5 sd
 %set_dir = 'wtest5/'; plot_name = 'full';  % plasma everywhere
-set_dir = 'constSZ_30/'; plot_name = 'constSZ_30um';  % plasma everywhere
+set_dir = 'hollow_60/'; plot_name = 'hollow_60um';  % plasma everywhere
 
 n0 = 1e17;
 [omega_p, lambda_p, skin_depth, plasma_time, plasma_period, E0] = plasma_parameters(n0);
