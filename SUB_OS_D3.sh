25cccfdd0776fd97838003ec54053bc2b2f2df5b
Spencer Gessner <sgess@login3.(none)>
2016-07-26 15:29:24 -0700

    fixed command writer

--- SUB_OS_D3.sh
+++ SUB_OS_D3.sh
@@ -14,7 +14,7 @@ echo Today is $day $month $year
 
 echo Creating Simulation directory and copying files now...
 
-cd /u/home/$fl/$me/d2scratch
+cd /u/d2scratch/$me
 mkdir $1
 cd $1
 
@@ -41,29 +41,29 @@ rt=$4
 
 # write file using above variables
 # NO TOUCHY!
-cat > osiris-2D.e.cmd << EOF
+cat > d3.osiris-2D.e.cmd << EOF
 #!/bin/csh -f
 #  osiris-2D.e.cmd
 #
-#  UGE job for osiris-2D.e built $DAY $month $day $TIME $zone $year 
+#  UGE job for osiris-2D.e built $DAY $month $day $TIME $zone $year
 #
 #  The following items pertain to this script
 #  Use current working directory
 #$ -cwd
 #  input           = /dev/null
-#  output          = /u/home/$fl/$me/d2scratch/$dirname/osiris-2D.e.joblog
-#$ -o /u/home/$fl/$me/d2scratch/$dirname/osiris-2D.e.joblog.\$JOB_ID
+#  output          = /u/d2scratch/$me/$dirname/osiris-2D.e.joblog
+#$ -o /u/d2scratch/$me/$dirname/osiris-2D.e.joblog.\$JOB_ID
 #  error           = Merged with joblog
 #$ -j y
 #  The following items pertain to the user program
-#  user program    = /u/home/$fl/$me/d2scratch/$dirname/osiris-2D.e
+#  user program    = /u/d2scratch/$me/$dirname/osiris-2D.e
 #  arguments       = 
 #  program input   = Specified by user program
 #  program output  = Specified by user program
 #  Parallelism:  $nodes-way parallel
 #  Resources requested
 #$ -pe dc* $nodes
-#$ -l h_data=${mem}M,h_rt=$rt:00:00,highp
+#$ -l h_data=${mem}M,h_rt=$rt:00:00
 # # #
 #
 #  Name of application for log
@@ -83,17 +83,17 @@ cat > osiris-2D.e.cmd << EOF
   set qqversion = 
   set qqapp     = "openmpi parallel"
   set qqptasks  = $nodes
-  set qqidir    = /u/home/$fl/$me/d2scratch/$dirname
+  set qqidir    = /u/d2scratch/$me/$dirname
   set qqjob     = osiris-2D.e
-  set qqodir    = /u/home/$fl/$me/d2scratch/$dirname
-  cd     /u/home/$fl/$me/d2scratch/$dirname
+  set qqodir    = /u/d2scratch/$me/$dirname
+  cd     /u/d2scratch/$me/$dirname
   source /u/local/bin/qq.sge/qr.runtime
   if (\$status != 0) exit (1)
 #
   echo "UGE job for osiris-2D.e built $DAY $month $day $TIME $zone $year"
   echo ""
   echo "  osiris-2D.e directory:"
-  echo "    "/u/home/$fl/$me/d2scratch/$dirname
+  echo "    "/u/d2scratch/$me/$dirname
   echo "  Submitted to UGE:"
   echo "    "\$qqsubmit
   echo "  'scratch' directory (on each node):"
@@ -112,15 +112,27 @@ cat > osiris-2D.e.cmd << EOF
   source /u/local/Modules/default/init/modules.csh
   module load intel/11.1
   module load openmpi/1.4
-  module load hdf5
 
-  echo osiris-2D.e -nt $nodes "" \>\& osiris-2D.e.output.\$JOB_ID
-  echo ""
 
   setenv PATH /u/local/bin:\$PATH
 
-  time \$MPI_BIN/mpiexec -n $nodes -machinefile \$QQ_NODEFILE  \\
-         /u/home/$fl/$me/d2scratch/$dirname/osiris-2D.e  >& /u/home/$fl/$me/d2scratch/$dirname/osiris-2D.e.output.\$JOB_ID 
+  if ( \$HOST =~  n[0-9]* ) then
+    set num=\`echo \$HOST | awk -F n '{print \$2}'\`
+    if ( \$num < 999 ) then
+	set btl_command="-mca btl tcp,sm,self"
+    else
+	set btl_command=""
+    endif
+  else
+        set btl_command=""
+  endif
+
+  echo \$MPI_BIN/mpiexec \$btl_command -n $nodes "" /u/d2scratch/$me/$dirname/osiris-2D.e \>\& osiris-2D.e.output.\$JOB_ID
+  echo ""
+
+#  time \$MPI_BIN/mpiexec  -n $nodes -machinefile \$QQ_NODEFILE  \\
+  time \$MPI_BIN/mpiexec \$btl_command -n $nodes -machinefile \$QQ_NODEFILE  \\
+         /u/d2scratch/$me/$dirname/osiris-2D.e  >& /u/d2scratch/$me/$dirname/osiris-2D.e.output.\$JOB_ID 
 
   echo ""
   echo "osiris-2D.e finished at:  "\` date \`
@@ -130,20 +142,22 @@ cat > osiris-2D.e.cmd << EOF
 #
   source /u/local/bin/qq.sge/qr.runtime
 #
-  echo "-------- /u/home/$fl/$me/d2scratch/$dirname/osiris-2D.e.joblog.\$JOB_ID --------" >> /u/local/apps/queue.logs/openmpi.log.parallel
- if (\`wc -l /u/home/$fl/$me/d2scratch/$dirname/osiris-2D.e.joblog.\$JOB_ID  | awk '{print \$1}'\` >= 1000) then
-        head -50 /u/home/$fl/$me/d2scratch/$dirname/osiris-2D.e.joblog.\$JOB_ID >> /u/local/apps/queue.logs/openmpi.log.parallel
+  echo "-------- /u/d2scratch/$me/$dirname/osiris-2D.e.joblog.\$JOB_ID --------" >> /u/local/apps/queue.logs/openmpi.log.parallel
+ if (\`wc -l /u/d2scratch/$me/$dirname/osiris-2D.e.joblog.\$JOB_ID  | awk '{print \$1}'\` >= 1000) then
+        head -50 /u/d2scratch/$me/$dirname/osiris-2D.e.joblog.\$JOB_ID >> /u/local/apps/queue.logs/openmpi.log.parallel
         echo " "  >> /u/local/apps/queue.logs/openmpi.log.parallel
-        tail -10 /u/home/$fl/$me/d2scratch/$dirname/osiris-2D.e.joblog.\$JOB_ID >> /u/local/apps/queue.logs/openmpi.log.parallel
+        tail -10 /u/d2scratch/$me/$dirname/osiris-2D.e.joblog.\$JOB_ID >> /u/local/apps/queue.logs/openmpi.log.parallel
   else
-        cat /u/home/$fl/$me/d2scratch/$dirname/osiris-2D.e.joblog.\$JOB_ID >> /u/local/apps/queue.logs/openmpi.log.parallel
+        cat /u/d2scratch/$me/$dirname/osiris-2D.e.joblog.\$JOB_ID >> /u/local/apps/queue.logs/openmpi.log.parallel
   endif
-#  cat            /u/home/$fl/$me/d2scratch/$dirname/osiris-2D.e.joblog.\$JOB_ID           >> /u/local/apps/queue.logs/openmpi.log.parallel
+#  cat            /u/d2scratch/$me/$dirname/osiris-2D.e.joblog.\$JOB_ID           >> /u/local/apps/queue.logs/openmpi.log.parallel
   exit (0)
 EOF
 
+chmod u+x d3.osiris-2D.e.cmd
+
 echo Submitting Job...
 
-#qsub osiris-2D.e.cmd
+qsub osiris-2D.e.cmd
 
 echo Job Submitted!
